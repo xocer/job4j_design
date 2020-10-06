6102ce71f828b280d5010efd7bbdec01e22f8973
xocer <vitamin9292@mail.ru>
2020-10-06 09:27:49 +0300

    5.1.1. Итератор для двухмерного массива int[][] [#380641]

--- src/main/java/ru/job4j/it/MatrixIt.java
+++ src/main/java/ru/job4j/it/MatrixIt.java
@@ -1,34 +1,44 @@
-//package ru.job4j.it;
-//
-//import java.util.Iterator;
-//import java.util.NoSuchElementException;
-//
-//public class MatrixIt implements Iterator<Integer> {
-//    private final int[][] data;
-//    private int row = 0;
-//    private int column = 0;
-//
-//    public MatrixIt(int[][] data) {
-//        this.data = data;
-//    }
-//
-//    @Override
-//    public boolean hasNext() {
-//        return row+column <= data.length;
-//    }
-//
-//    @Override
-//    public Integer next() {
-//        if (!hasNext()) {
-//            throw new NoSuchElementException();
-//        }
-//        if (column < data.length) {
-//            return data[row][column++];
-//        } else return data[++row][column = 0];
-//    }
-//
-//    @Override
-//    public void remove() {
-//
-//    }
-//}
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MatrixIt implements Iterator<Integer> {
+    private final int[][] data;
+    private int row = 0;
+    private int column = 0;
+
+    public MatrixIt(int[][] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (data[row].length == 0 && data.length == 1) {
+            return false;
+        }
+        return row < data.length;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        if (column < data[row].length) {
+            return data[row][column++];
+        } else {
+            row++;
+            column = 0;
+            while (data[row].length == 0) {
+                row++;
+            }
+            return data[row][column++];
+        }
+    }
+
+    @Override
+    public void remove() {
+
+    }
+}
