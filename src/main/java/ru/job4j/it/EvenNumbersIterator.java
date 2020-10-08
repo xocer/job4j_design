1bcd2fa9ba277178846e7ed205a03ff6e6a93bb0
xocer <vitamin9292@mail.ru>
2020-10-08 11:43:28 +0300

    0. Что такое обобщенные типы (generics) [#380654]

--- src/main/java/ru/job4j/it/EvenNumbersIterator.java
+++ src/main/java/ru/job4j/it/EvenNumbersIterator.java
@@ -1,6 +1,8 @@
 package ru.job4j.it;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 public class EvenNumbersIterator implements Iterator<Integer> {
