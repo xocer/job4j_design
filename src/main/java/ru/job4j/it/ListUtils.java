220de894aa7a7253edf9f6f661f151b995ee04ec
grishinv <vitamin9292@mail.ru>
2020-10-15 20:43:21 +0300

    7. ListIterator [#380630]

---
+++ src/main/java/ru/job4j/it/ListUtils.java
@@ -0,0 +1,77 @@
+package ru.job4j.it;
+
+import java.util.*;
+import java.util.function.Predicate;
+
+public class ListUtils {
+
+    public static <T> void addBefore(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> i = list.listIterator();
+        while (i.hasNext()) {
+            if (i.nextIndex() == index) {
+                i.add(value);
+                break;
+            }
+            i.next();
+        }
+    }
+
+    public static <T> void addAfter(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> i = list.listIterator();
+        while (i.hasNext()) {
+            if (i.nextIndex() == index) {
+                i.next();
+                i.add(value);
+                break;
+            }
+            i.next();
+        }
+    }
+
+    public static <T> List<T> removeIf(List<T> list, Predicate<T> filter) {
+        ListIterator<T> i = list.listIterator();
+        while (i.hasNext()) {
+            if (filter.test(i.next())) {
+                i.remove();
+            }
+        }
+        return list;
+    }
+
+    public static <T> List<T> replaceIf(List<T> list, Predicate<T> filter, T value) {
+        ListIterator<T> i = list.listIterator();
+        while (i.hasNext()) {
+            if (filter.test(i.next())) {
+                i.set(value);
+            }
+        }
+        return list;
+    }
+
+    public static <T> List<T> removeAll(List<T> list, List<T> elements) {
+        ListIterator<T> i = list.listIterator();
+
+        while (i.hasNext()) {
+            T iList = i.next();
+            for (int j = 0; j < elements.size(); j++) {
+                if (iList.equals(elements.get(j))) {
+                    i.remove();
+                }
+            }
+        }
+        return list;
+    }
+
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7));
+        List<Integer> elements = new ArrayList<>(Arrays.asList(11, 2, 33, 4, 55, 65, 7));
+
+
+        list = removeAll(list, elements);
+        System.out.println(list);
+    }
+
+}
---
+++ src/test/java/ru/job4j/it/ListUtilsTest.java
@@ -0,0 +1,65 @@
+package ru.job4j.it;
+
+import org.hamcrest.core.Is;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static org.junit.Assert.assertThat;
+
+public class ListUtilsTest {
+
+    @Test
+    public void whenAddBefore() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(1, 3));
+        ListUtils.addBefore(input, 1, 2);
+        assertThat(Arrays.asList(1, 2, 3), Is.is(input));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenAddBeforeWithInvalidIndex() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(1, 3));
+        ListUtils.addBefore(input, 3, 2);
+    }
+
+    @Test
+    public void whenAddAfter() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(1, 3));
+        ListUtils.addAfter(input, 0, 2);
+        assertThat(Arrays.asList(1, 2, 3), Is.is(input));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenAddAfterWithInvalidIndex() {
+        List<Integer> input = new ArrayList<>(Arrays.asList(1, 3));
+        ListUtils.addAfter(input, 2, 2);
+    }
+
+    @Test
+    public void whenRemoveIf() {
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3));
+        Predicate<Integer> predicate = (i) -> i == 2;
+        list = ListUtils.removeIf(list, predicate);
+        assertThat(Arrays.asList(1,3), Is.is(list));
+    }
+
+    @Test
+    public void whenReplaceIf() {
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 2));
+        Predicate<Integer> predicate = (i) -> i == 2;
+        list = ListUtils.replaceIf(list, predicate, 5);
+        assertThat(Arrays.asList(1,5, 3, 4, 5), Is.is(list));
+    }
+
+    @Test
+    public void whenRemoveAll() {
+        List<Integer> list = new ArrayList<>(Arrays.asList(11, 22, 33, 44));
+        List<Integer> elements = new ArrayList<>(Arrays.asList(33));
+        list = ListUtils.removeAll(list, elements);
+        assertThat(Arrays.asList(11, 22, 44), Is.is(list));
+    }
+
+}
