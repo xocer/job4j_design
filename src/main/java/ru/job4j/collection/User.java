27696674fd411e367d354153f86a000ef3f7911f
grishinv <vitamin9292@mail.ru>
2020-10-19 12:04:39 +0300

    4. Переопределить только equals [#380662]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -17,9 +17,19 @@ public class User {
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(name, children, birthday);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return children == user.children &&
+                name.equals(user.name) &&
+                birthday.equals(user.birthday);
     }
+
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(name, children, birthday);
+//    }
 
     public static void main(String[] args) {
         Map<User, Object> map = new HashMap<User, Object>();
