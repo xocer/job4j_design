9dc9fc326e5eb8131b2dd15f625890acc3d5078a
grishinv <vitamin9292@mail.ru>
2020-10-27 10:42:17 +0300

    0.1. FileOutputStream. [#380679]

---
+++ src/main/java/ru/job4j/input_output/MultiplicationTable.java
@@ -0,0 +1,24 @@
+package ru.job4j.input_output;
+
+import java.io.FileOutputStream;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        MultiplicationTable multiplicationTable = new MultiplicationTable();
+        multiplicationTable.writeInTableMultipl(10, 10);
+
+    }
+
+    public void writeInTableMultipl(int q, int a) {
+        try (FileOutputStream fos = new FileOutputStream("result")) {
+            for (int i = 1; i < q; i++) {
+                for (int j = 1; j < a; j++) {
+                    fos.write((i + " * " + j + " = " + i * j + "\n").getBytes());
+                }
+                fos.write("\n".getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
