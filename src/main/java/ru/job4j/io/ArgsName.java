97cffce3ae24862d984fcf0c273094b7f49635af
grishinv <vitamin9292@mail.ru>
2020-11-04 13:02:28 +0300

    5.1. Именованные аргументы [#380683]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,37 @@
+package ru.job4j.io;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < args.length; i++) {
+            String[] tmp = args[i].split("[-=]");
+            values.put(tmp[1], tmp[2]);
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
