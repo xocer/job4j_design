a6f75e1e8f11a09f96d916211cf9e35ecb3a8008
grishinv <vitamin9292@mail.ru>
2020-11-04 11:07:00 +0300

    5. Валидация параметров запуска. [#380678] 2.0

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -8,11 +8,11 @@ import java.util.List;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
+        if (args.length < 2) {
             throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
         }
 
-        Path start = Paths.get(args[0]);
+        Path start = Paths.get("C:\\Program Files\\");
         search(start, args[1]).forEach(System.out::println);
     }
 
