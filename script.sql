af154675780216e03de53845b4695a246d4c12b9
grishinv <vitamin9292@mail.ru>
2020-11-28 10:44:28 +0300

    1. Схема прав пользователи и роли. [#380608]

--- script.sql
+++ script.sql
@@ -2,7 +2,8 @@ create database treker_db;
 
 create table users(
     id serial primary key,
-    name text
+    name text,
+    role_id int REFERENCES role(id)
 );
 
 create table role (
@@ -15,19 +16,30 @@ create table rules (
     name text
 );
 
+create table role_rules (
+    id SERIAL PRIMARY KEY ,
+    role_id int REFERENCES role(id),
+    rules_id int REFERENCES rules(id)
+);
+
 create table item (
     id serial primary key ,
-    name text
+    name text ,
+    user_id int REFERENCES users(id),
+    category_id int REFERENCES category(id),
+    state_id int REFERENCES state(id)
 );
 
 create table comments (
     id serial primary key ,
-    name text
+    name text ,
+    item_id int REFERENCES item(id)
 );
 
 create table attachs (
     id serial primary key ,
-    name text
+    name text,
+    item_id int REFERENCES item(id)
 );
 
 create table category (
