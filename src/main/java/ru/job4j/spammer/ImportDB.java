209002cf33d7297178ad2a8c53e6e073ae3b0038
grishinv <vitamin9292@mail.ru>
2020-12-03 12:51:22 +0300

    3. Загрузка базы спамеров. [#380580]

---
+++ src/main/java/ru/job4j/spammer/ImportDB.java
@@ -0,0 +1,86 @@
+package ru.job4j.spammer;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class ImportDB {
+
+    private Properties cfg;
+    private String dump;
+
+    public ImportDB(Properties cfg, String dump) {
+        this.cfg = cfg;
+        this.dump = dump;
+    }
+
+    public List<User> load() throws IOException {
+        List<User> users = new ArrayList<>();
+        try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
+            String[] line = null;
+            while (rd.ready()) {
+                line = rd.readLine().split(";");
+                users.add(new User(line[0], line[1]));
+            }
+        }
+        return users;
+    }
+
+    public void save(List<User> users) throws ClassNotFoundException, SQLException {
+        Class.forName(cfg.getProperty("jdbc.driver"));
+        try (Connection cnt = DriverManager.getConnection(
+                cfg.getProperty("jdbc.url"),
+                cfg.getProperty("jdbc.username"),
+                cfg.getProperty("jdbc.password")
+        )) {
+            for (User user : users) {
+                try (PreparedStatement ps = cnt.prepareStatement(
+                        "insert into users (name, email) values (?, ?)")) {
+                    ps.setString(1, user.getName());
+                    ps.setString(2, user.getEmail());
+                    ps.execute();
+                }
+            }
+        }
+    }
+
+    private static class User {
+        private String name;
+        private String email;
+
+        public User(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Properties cfg = new Properties();
+        try (FileInputStream in = new FileInputStream("src/main/resources/app.properties")) {
+            cfg.load(in);
+        }
+        ImportDB db = new ImportDB(cfg, "404.txt");
+        db.save(db.load());
+    }
+}
