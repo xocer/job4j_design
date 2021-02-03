e3611b25fcd7b5eb68a7d55f826cbb84cc2cfd7c
xocer <vitamin9292@mail.ru>
2021-02-03 18:29:14 +0300

    4. JAXB. Преобразование XML в POJO. [#380694]

--- src/main/java/ru/job4j/io/xml/Car.java
+++ src/main/java/ru/job4j/io/xml/Car.java
@@ -1,11 +1,37 @@
 package ru.job4j.io.xml;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.Arrays;
+
+@XmlRootElement(name = "car")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Car {
-    private final boolean isTruck;
-    private final int yearOfManufacture;
-    private final String modelName;
-    private final Number number;
-    private final String[] functions;
+    @XmlAttribute
+    private boolean isTruck;
+
+    @XmlAttribute
+    private int yearOfManufacture;
+
+    @XmlAttribute
+    private String modelName;
+
+    private Number number;
+
+    @XmlElementWrapper(name = "functions")
+    @XmlElement(name = "function")
+    private String[] functions;
+
+    public Car() {
+
+    }
 
     public Car(boolean isTruck, int yearOfManufacture,
                String modelName, Number number, String[] functions) {
@@ -16,8 +42,32 @@ public class Car {
         this.functions = functions;
     }
 
-    public static void main(String[] args) {
-        new Car(false, 2020, "BMW",
-                new Number("C507AM"), new String[] {"подогрев руля", "стеклоподъемники"});
+    @Override
+    public String toString() {
+        return "Car{"
+                + "isTruck=" + isTruck
+                + ", yearOfManufacture=" + yearOfManufacture
+                + ", modelName='" + modelName + '\''
+                + ", number=" + number
+                + ", functions=" + Arrays.toString(functions)
+                + '}';
+    }
+
+    public static void main(String[] args) throws JAXBException, IOException {
+        Car car = new Car(false, 2020, "BMW",
+                new Number("C507AM"), new String[]{"подогрев руля", "стеклоподъемники"});
+
+        JAXBContext context = JAXBContext.newInstance(Car.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        StringWriter writer = new StringWriter();
+        marshaller.marshal(car, writer);
+        String xml = writer.getBuffer().toString();
+        System.out.println(xml);
+
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        StringReader reader = new StringReader(xml);
+        Car result = (Car) unmarshaller.unmarshal(reader);
+        System.out.println(result);
     }
 }
--- src/main/java/ru/job4j/io/xml/Number.java
+++ src/main/java/ru/job4j/io/xml/Number.java
@@ -1,9 +1,26 @@
 package ru.job4j.io.xml;
 
+import com.sun.xml.txw2.annotation.XmlElement;
+
+import javax.xml.bind.annotation.XmlAttribute;
+
+@XmlElement(value = "car")
 public class Number {
-    private final String id;
+    @XmlAttribute
+    private String id;
+
+    public Number() {
+
+    }
 
     public Number(String number) {
         this.id = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Number{"
+                + "id='" + id + '\''
+                + '}';
     }
 }
